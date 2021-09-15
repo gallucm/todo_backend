4cc9a3bb85858282eb421137212cccaa5f0b6b87
gallucm <gallucciocristian@gmail.com>
2021-09-14 21:11:41 -0300

    fix to keep folders

--- app.js
+++ app.js
@@ -6,4 +6,26 @@ app.use(cors());
 
 app.options('*', cors());
 
+var app = express();
+
+var cors = require('cors');
+
+// Archivo de rutas
+
+// Middleware
+app.use(cors());
+
+// Cabeceras y CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization,  X-Requested-With, X-API-KEY, Origin, Accept, Access-Control-Allow-Request-Method');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.options('*', cors())
+
+// Rutas
+
 module.exports = app;
