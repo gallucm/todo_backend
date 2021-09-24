b9735bb7d6dec9aaf8db8e6cdb8efcece5d98392
Cristian Galluccio <gallucciocristian@gmail.com>
2021-09-23 21:19:01 -0300

    remove fix date

--- helpers/date.js
+++ helpers/date.js
@@ -1,6 +1,7 @@
 const getDate = () => {
     const date = new Date();
-    return date.setHours(date.getHours() - 3);
+    // return date.setHours(date.getHours() - 3);
+    return date;
 }
 
 module.exports = {
